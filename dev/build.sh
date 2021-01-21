c884cd7c077abd06ee68289632784a2a981214cc
IjorTengab <m_roji28@yahoo.com>
2021-01-22 04:29:56 +0700

    Update file 'build.sh' and '.gitignore'.

--- dev/build.sh
+++ dev/build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+
 # Generate code.
 chmod +x rcm.options.sh
 . rcm.options.sh
+# Create File.
 touch ../rcm.sh
 chmod +x ../rcm.sh
 SOURCE=$(<rcm.dev.sh)
+# Replace line.
 FILE_PARSE_OPTIONS=$(<rcm.parse_options.sh)
 FILE_FUNCTIONS=$(<rcm.functions.sh)
 SOURCE="${SOURCE//source \$(dirname \$0)\/rcm.parse_options.sh/$FILE_PARSE_OPTIONS}"
 SOURCE="${SOURCE//source \$(dirname \$0)\/rcm.functions.sh/$FILE_FUNCTIONS}"
+# Dump.
 echo "${SOURCE}" > ../rcm.sh
 # Delete line.
 sed -i '/var-dump\.function\.sh/d' ../rcm.sh
